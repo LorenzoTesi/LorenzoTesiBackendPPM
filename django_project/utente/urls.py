b70dde5eb3089550712dd05b6fc01f5c4ccf7db8
Lollo <lorenzotesi7@gmail.com>
2026-06-28 22:00:34 +0200

    Aggiornata logica di login, ora due pagine una per partecipante ed una per organizzatore

--- django_project/utente/urls.py
+++ django_project/utente/urls.py
@@ -3,5 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import path
 
+from . import views
+
+app_name = 'utente'
+
 urlpatterns = [
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.ParticipantLoginView.as_view(),
+         name='login_attendee'),
+    path('organizzatore/login/', views.OrganizerLoginView.as_view(),
+         name='login_organizer'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- django_project/utente/views.py
+++ django_project/utente/views.py
@@ -1,49 +1,76 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.core.exceptions import ValidationError
 from django.contrib import messages
+from django.shortcuts import redirect
 from .models import CustomUser
 
+#TODO implementare eliminazione dell'account e update dei campi dell'utente, logica per rendere un prfilo un organizatore oppure registrne uno ex novo
+
+# Form registrazione
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('email', 'is_organizer',)
+        fields = UserCreationForm.Meta.fields + ('email',)
 
-def register_view(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "Registrazione avvenuta con successo!")
-            return redirect('eventi:lista_eventi')
-        else:
-            messages.error(request, "Errore nella registrazione. Riprova.")
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'utente/register.html', {'form': form})
+    # is_organizer viene forzato a False nel save
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_organizer = False
+        if commit:
+            user.save()
+        return user
 
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.success(request, f"Bentornato, {username}!")
-                return redirect('eventi:lista_eventi')
-        messages.error(request, "Username o password non validi.")
-    else:
-        form = AuthenticationForm()
-    return render(request, 'utente/login.html', {'form': form})
+
+# AuthenticationForm per partecipanti
+class ParticipantAuthForm(AuthenticationForm):
+    def confirm_login_allowed(self, user):
+        super().confirm_login_allowed(user)
+
+
+
+# AuthenticationForm per organizzatori
+class OrganizerAuthForm(AuthenticationForm):
+    def confirm_login_allowed(self, user):
+        super().confirm_login_allowed(user)
+        if not user.is_organizer:
+            raise ValidationError(
+                "Non sei un organizzatore, usa il login partecipanti."
+            )
+
+
+# LoginView per partecipanti
+class ParticipantLoginView(LoginView):
+    form_class = ParticipantAuthForm
+    template_name = 'utente/login_attendee.html'
+
+    def get_success_url(self):
+        return reverse_lazy('eventi:lista_eventi')
+
+
+# LoginView per organizzatori
+class OrganizerLoginView(LoginView):
+    form_class = OrganizerAuthForm
+    template_name = 'utente/login_organizer.html'
+
+    def get_success_url(self):
+        return reverse_lazy('eventi:lista_organizzatore')
+
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'utente/register.html'
+    success_url = reverse_lazy('utente:login_attendee')
+
 
 def logout_view(request):
     logout(request)
     messages.info(request, "Hai effettuato il logout.")
-    return redirect('utente:login')
+    return redirect('utente:login_partecipante')
+
+
+
